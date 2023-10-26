7f80d907437b35d7e2adfd4d4f0038532e2921f5
matheusguim21 <matheusguim13@gmail.com>
2023-10-25 23:07:52 -0300

    Enums

---
+++ src/enumeracoes/application/Program.java
@@ -0,0 +1,13 @@
+package enumeracoes.application;
+
+import enumeracoes.entities.Order;
+import enumeracoes.enums.OrderStatus;
+
+import java.util.Date;
+
+public class Program {
+	public static void main(String[] args) {
+		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+		System.out.printf(order.toString());
+	}
+}

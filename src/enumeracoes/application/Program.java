3a9d0df96996d403d17301006c3092918bafca36
Matheus Guimarães <matheusguim@MacBook-Pro-de-Matheus.local>
2023-10-26 22:32:19 -0300

    Lidando com arquivos

--- src/enumeracoes/application/Program.java
+++ src/enumeracoes/application/Program.java
@@ -5,9 +5,11 @@ import enumeracoes.enums.OrderStatus;
 
 import java.util.Date;
 
+
 public class Program {
 	public static void main(String[] args) {
 		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
 		System.out.printf(order.toString());
+
 	}
 }
